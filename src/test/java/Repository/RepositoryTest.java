6f8b711f6c92b0311dec741aa7d852d98284cd5b
unknown <ladiladi85@gmail.com>
2019-03-15 18:15:24 +0200

    add project to repository

--- src/test/java/Repository/RepositoryTest.java
+++ src/test/java/Repository/RepositoryTest.java
@@ -1,4 +1,4 @@
-package 931_AndrasiBarboi;
+package Repository;
 
 import static org.junit.Assert.assertTrue;
 
@@ -7,7 +7,7 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class RepositoryTest
 {
     /**
      * Rigorous Test :-)
